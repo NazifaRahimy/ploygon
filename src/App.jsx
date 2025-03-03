d43a3809f922617d88a2766759b54a6bd362604f
nazifaRahimi <nazifarahimy6@gmail.com>
2025-03-02 23:13:01 -0800

    add home of part to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./comm/NavBar"
+import Home from "./comm/Home"
 function App() {
 
   return (
     < div className="bg-[#090218]">
-    < Navbar />
+      < Navbar />
+      <Home />
     </div>
   )
 }
