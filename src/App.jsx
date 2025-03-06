fa0e45d03e6452bf51e62bfd97338a606bea861d
nazifaRahimi <nazifarahimy6@gmail.com>
2025-03-06 00:11:41 -0800

    Add Header component with Tailwind styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
+import Navbar from "./comm/NavBar"
 
 function App() {
-
-
   return (
-    <h1 className="text-3xl font-bold underline uppercase text-blue-400 m-10 hover:text-red-400">
-    Hello world!
-  </h1>
+    < div className="bg-[#090218]">
+        <Navbar />
+    </div>
   )
 }
 
