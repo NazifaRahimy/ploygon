fb33bfab5d169c50802b6c4e48954bada031e703
nazifaRahimi <nazifarahimy6@gmail.com>
2025-03-07 21:44:10 -0800

    Add Footer component with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import Speaker from "./comm/Speaker"
 import Sponsor from "./comm/Sponsor";
 import Organizer from "./comm/Organizer";
 import Contact from "./comm/Contact";
-
+import { useState } from "react";
 
 function App() {
+  const [click ,setClick] = useState(false)
   return (
     < div className="bg-[#020618]">
         <Navbar />
@@ -17,6 +18,12 @@ function App() {
         <Sponsor />
         <Organizer />
         <Contact />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <button  onClick={()=>setClick(!click)}
+        className='button w-1/2 h-20 flex items-center mx-auto justify-center text-3xl font-bold duration-300 bg-purple-500 hover:bg-purple-600 text-white text-center  le'>{click ? 'be the best always' : 'Click me!'}</button>
     </div>
   )
 }
